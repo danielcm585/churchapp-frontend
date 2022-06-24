04bc16f7b562602002a32f2f8831f9a1b59c3b2f
danielcm585 <danielcm585@gmail.com>
2022-06-24 16:22:56 +0700

    created profile details screen

---
+++ src/screens/ProfileDetailsScreen.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { Appbar } from '../components'
+
+import { Avatar, HStack, Text, VStack } from 'native-base'
+
+export default function ProfileDetailsScreen({ route, navigation }) {
+  const { profile } = route.params
+
+  const months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
+  const birthYear = parseInt(profile.birth.split('-')[0])
+  const birthMonth = months[parseInt(profile.birth.split('-')[1])]
+  const birthDate = parseInt(profile.birth.split('-')[2].split('T')[0])
+
+  return (
+    <>
+      <Appbar title={profile.username} profile={profile} navigation={navigation} />
+      <HStack mx='4' mt='4' alignItems='center'>
+        <Avatar size='xl' source={{ uri: profile.photo }} />
+        <VStack ml='4'>
+          <Text fontSize='lg' fontWeight='bold'>{profile.name}</Text>
+          <Text fontSize='sm' fontWeight='semibold'>{profile.phone}</Text>
+          <Text fontSize='sm' fontWeight='semibold'>{birthDate} {birthMonth} {birthYear}</Text>
+        </VStack>
+      </HStack>
+    </>
+  )
+}
